b711da081a16485337489b118a7c2dc9d9dd0f86
RanugiJang <ranugigihansa@gmail.com>
2026-01-13 19:07:55 +0530

    Create register.html

---
+++ src/main/java/com/luv2code/jobportal/controller/UsersController.java
@@ -0,0 +1,32 @@
+package com.luv2code.jobportal.controller;
+
+import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.entity.UsersType;
+import com.luv2code.jobportal.services.UsersTypeService;
+import org.hibernate.usertype.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UsersController {
+
+    private final UsersTypeService usersTypeService;
+
+    @Autowired
+    public UsersController(UsersTypeService usersTypeService){
+        this.usersTypeService = usersTypeService;
+    }
+
+    @GetMapping("/register")
+    public String register(Model model){
+        List<UsersType> userTypes = usersTypeService.getAll();
+        model.addAttribute("getAllTypes", userTypes);
+        model.addAttribute("user", new Users());
+        return "register";
+
+    }
+}
